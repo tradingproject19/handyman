222d2ae5f375173e144c87812ee7a9c2edfa8ccc
tradingproject19 <info@flowtrade.com>
2023-07-06 11:39:14 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'handyman',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
